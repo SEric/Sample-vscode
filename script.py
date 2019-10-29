0f305ad15230b851c65ce2be53db01ecc0046b96
bigplot <sch.kerner@googlemail.com>
2019-10-29 11:14:18 +0100

    Removed some Code

--- script.py
+++ script.py
@@ -1,17 +1,5 @@
-import math
-import sys
-from os import rename
-
 import requests
 
-name = input("Your Name?")
-
-
-def greet(who_to_greet):
-    return f"Hello, {who_to_greet}!"
-
-
-print(greet(name))
-
-# r = requests.get("https://github.io")
-# print(r.status_code)
+r = requests.get("https://github.io")
+print(r.status_code)
+print(r.ok)
